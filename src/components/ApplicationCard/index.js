467609c8937a8c13ecd8cf1590b1bb3f12487ff4
Omri Gazitt <ogazitt@gmail.com>
2021-02-14 17:16:41 -0800

    fixed syntax error

--- src/components/ApplicationCard/index.js
+++ src/components/ApplicationCard/index.js
@@ -4,7 +4,7 @@ import appicon from './application.svg'
 import './ApplicationCard.css'
 
 const ApplicationCard = ({application, onClick, ...props}) => 
-  <Card className='application-card' onClick={onClick} props={...props}>
+  <Card className='application-card' onClick={onClick} {...props}>
     <div className='display-flex'>
       <Card.Img src={appicon} alt="logo" />
       <Card.Title>{application}</Card.Title>
