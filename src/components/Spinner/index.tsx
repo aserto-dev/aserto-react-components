a024d7d6789be3d34ffb22c9f87a10783dd47b61
Eduardo Pedroso <drdpedroso@gmail.com>
2021-03-18 19:35:19 -0300

    Create spinner (#8)

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+const Waves = keyframes`
+  0%   { top: 0;     opacity: 1; }
+  50%  { top: 30px;  opacity: .2; }
+  100% { top: 0;     opacity: 1; }
+`
+
+const Container = styled.div`
+  width: 80px;
+  height: 100px;
+  margin: 100px auto;
+  margin-top: calc(100vh / 2 - 50px);
+  div {
+    position: relative;
+    box-sizing: border-box;
+    float: left;
+    margin: 0 10px 10px 0;
+    width: 12px;
+    height: 12px;
+    border-radius: 18px;
+    background: #fff;
+  }
+  div:nth-child(4n + 1) {
+    animation: ${Waves} 2s ease 0s infinite;
+  }
+  div:nth-child(4n + 2) {
+    animation: ${Waves} 2s ease 0.2s infinite;
+  }
+  div:nth-child(4n + 3) {
+    animation: ${Waves} 2s ease 0.4s infinite;
+  }
+  div:nth-child(4n + 4) {
+    animation: ${Waves} 2s ease 0.6s infinite;
+    margin-right: 0;
+  }
+`
+
+const Spinner: React.FC = () => {
+  return (
+    <Container>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </Container>
+  )
+}
+
+export { Spinner }
---
+++ src/stories/Spinner.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import { Spinner } from '../components/common/Spinner'
+
+export default {
+  title: 'Common/Spinner',
+  component: Spinner,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta
+
+const Template: Story<any> = (args) => <Spinner {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {}
