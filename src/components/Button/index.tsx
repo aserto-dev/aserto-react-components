d41a0bdcacc61b99e67037be024d8ee6e3821fa2
Eduardo <drdpedroso@gmail.com>
2021-03-27 09:58:52 -0300

    Add new secondary-borderless variant to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { ButtonProps as BootstrapButtonProps } from 'react-bootstrap'
-import { PrimaryButton, SecondaryButton, DangerButton } from './styles'
+import { PrimaryButton, SecondaryButton, DangerButton, SecondaryBorderlessButton } from './styles'
 
 export interface ButtonProps extends BootstrapButtonProps {
-  variant?: 'primary' | 'secondary' | 'danger'
+  variant?: 'primary' | 'secondary' | 'danger' | 'secondary-borderless'
 }
 
 const getButtonFromVariant = (variant: string) => {
@@ -11,6 +11,7 @@ const getButtonFromVariant = (variant: string) => {
     primary: PrimaryButton,
     secondary: SecondaryButton,
     danger: DangerButton,
+    'secondary-borderless': SecondaryBorderlessButton,
   }
   return variantObj[variant] || variantObj.primary
 }
