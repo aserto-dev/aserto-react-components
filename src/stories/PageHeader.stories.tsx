7a464e6adb1f4ca36663c5fce63f882e37df30fb
Eduardo <drdpedroso@gmail.com>
2021-03-30 19:05:02 -0300

    Create pagination and change user card component

--- src/stories/PageHeader.stories.tsx
+++ src/stories/PageHeader.stories.tsx
@@ -1,35 +1,13 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { PageHeader, PageHeaderProps } from '../components/PageHeader'
-import { Breadcrumb } from '../components/Breadcrumb'
+import { Pagination, PaginationProps } from '../components/Pagination'
 
 export default {
-  title: 'Common/PageHeader',
-  component: PageHeader,
+  title: 'Common/Pagination',
+  component: Pagination,
 } as Meta
 
-const Template: Story<PageHeaderProps> = (args) => <PageHeader {...args} />
+const Template: Story<PaginationProps> = (args) => <Pagination {...args} />
 
 export const Primary = Template.bind({})
-Primary.args = {
-  title: 'Page title',
-}
-
-export const PrimaryWithLoading = Template.bind({})
-PrimaryWithLoading.args = {
-  title: 'Page title',
-  loading: true,
-}
-
-export const PrimaryWithLoadFunction = Template.bind({})
-PrimaryWithLoadFunction.args = {
-  title: 'Page title',
-  load: console.log,
-}
-
-export const PrimaryWithBreadcrumb = Template.bind({})
-PrimaryWithBreadcrumb.args = {
-  children: (
-    <Breadcrumb breadcrumbText="Applications" title="Create application" breadcrumbUrl="/" />
-  ),
-}
+Primary.args = {}
