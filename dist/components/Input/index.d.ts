cf3d589f4a1714a6eeefac975d4ee2b4b4947be0
Eduardo <drdpedroso@gmail.com>
2021-04-24 11:04:40 -0300

    .

---
+++ dist/components/Input/index.d.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+export declare type InputProps = {
+    placeholder?: string;
+    value?: string | number;
+    onChange: (e: any) => void;
+    label?: string;
+    info?: string;
+    error?: string;
+    isValid?: boolean;
+    isUnavailable?: boolean;
+    disabled?: boolean;
+    type?: 'text' | 'number' | 'password' | null;
+    style?: any;
+    hasSmallLabel?: boolean;
+};
+export declare const Input: React.ForwardRefExoticComponent<InputProps & React.RefAttributes<any>>;
