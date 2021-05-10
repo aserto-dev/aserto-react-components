7169b738162079ebe34c19304dd0d4e26e849aad
Eduardo <drdpedroso@gmail.com>
2021-05-10 19:58:24 -0300

    Add controless select

---
+++ src/components/SelectWithoutControl/index.tsx
@@ -0,0 +1,186 @@
+import React, { useCallback } from 'react'
+import ReactSelect, { components } from 'react-select'
+import { theme } from '../../theme'
+import { Label } from '../Label'
+
+export type SelectOption = {
+  value: string | number
+  label: string
+  shouldStopPropagation?: string
+  onClick?: () => void
+}
+
+export type SelectWithoutControlProps = {
+  options: Array<SelectOption>
+  defaultValue?: SelectOption
+  onChange: (e: any) => void
+  disabled?: boolean
+  label?: string
+  isLoading?: boolean
+  value?: SelectOption | null
+  style?: any
+  disableLabel?: boolean
+  name?: string
+}
+
+const groupStyles = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: 0,
+  marginTop: -8,
+  marginBottom: -3,
+  marginLeft: -11,
+  marginRight: -11,
+  height: 1,
+  backgroundColor: theme.grey,
+}
+
+const formatGroupLabel = () => <div style={groupStyles} />
+
+export const SelectWithoutControl = React.forwardRef<any, SelectWithoutControlProps>(
+  (
+    {
+      options,
+      defaultValue,
+      onChange,
+      label,
+      disabled,
+      isLoading,
+      style,
+      value,
+      disableLabel,
+      name,
+      ...props
+    },
+    forRef
+  ) => {
+    const removeFocusBox = {
+      outline: 'none',
+      webkitBoxShadow: 'none',
+      boxShadow: 'none',
+    }
+
+    const Option = useCallback((props) => {
+      return (
+        <div>
+          <components.Option
+            {...props}
+            innerProps={{
+              ...props.innerProps,
+              onMouseDown: (e) => {
+                if (props.data.shouldStopPropagation) {
+                  e.stopPropagation()
+                  props.data?.onClick()
+                }
+              },
+            }}
+          />
+        </div>
+      )
+    }, [])
+
+    const colourStyles = {
+      control: (styles, { isDisabled, isFocused }) => {
+        return {
+          ...styles,
+          backgroundColor: 'transparent',
+          color: theme.grey70,
+          border: 'none',
+          opacity: isDisabled ? 0.6 : 1,
+          outline: isFocused ? 'none' : '',
+          boxShadow: 'none',
+          borderWidth: 1,
+          ':hover': {
+            ...styles[':hover'],
+            backgroundColor: theme.grey10,
+            borderColor: theme.lochivarAccent1,
+            color: theme.grey100,
+          },
+        }
+      },
+      option: (styles, { isDisabled, isFocused, isSelected }) => {
+        return {
+          ...styles,
+          backgroundColor: isDisabled
+            ? theme.grey20
+            : isFocused
+            ? theme.grey30
+            : isSelected
+            ? theme.grey20
+            : theme.grey20,
+          borderLeft: isSelected ? `5px solid ${theme.lochivarAccent3}` : '',
+          color: isFocused ? theme.grey100 : theme.grey70,
+          height: 36,
+          fontSize: 14,
+          cursor: isDisabled ? 'not-allowed' : 'default',
+          ':active': {
+            ...styles[':active'],
+            backgroundColor: theme.grey30,
+          },
+        }
+      },
+
+      input: (styles) => {
+        return {
+          ...styles,
+          color: theme.grey100,
+          borderColor: theme.grey60,
+        }
+      },
+      placeholder: (styles, { isDisabled }) => ({
+        ...styles,
+        color: isDisabled ? theme.grey40 : theme.grey90,
+      }),
+      singleValue: (styles, { isDisabled }) => ({
+        ...styles,
+        color: isDisabled ? theme.grey40 : theme.grey100,
+        ...removeFocusBox,
+      }),
+      menu: (styles) => ({
+        ...styles,
+        backgroundColor: 'transparent',
+        zIndex: 6,
+        marginTop: -5,
+      }),
+      dropdownIndicator: (styles, { isDisabled }) => ({
+        ...styles,
+        color: isDisabled ? theme.grey40 : theme.grey70,
+      }),
+      menuList: (style) => ({
+        ...style,
+        zIndex: 5,
+        borderRadius: 6,
+      }),
+      indicatorSeparator: (styles) => ({
+        ...styles,
+        display: 'none',
+      }),
+    }
+    return (
+      <div style={style}>
+        {label && (
+          <Label htmlFor={name} disabled={disableLabel}>
+            {label}
+          </Label>
+        )}
+        <ReactSelect
+          {...props}
+          name={name}
+          isClearable={false}
+          inputId={name}
+          isSearchable={false}
+          ref={forRef || null}
+          isLoading={isLoading}
+          isDisabled={disabled}
+          options={options}
+          defaultValue={defaultValue}
+          onChange={onChange}
+          styles={colourStyles}
+          formatGroupLabel={formatGroupLabel}
+          components={{ Option }}
+        />
+      </div>
+    )
+  }
+)
---
+++ src/stories/SelectWithoutControl.stories.tsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react/types-6-0'
+import { SelectWithoutControl, SelectWithoutControlProps } from '../components/SelectWithoutControl'
+
+export default {
+  title: 'Common/SelectWithoutControl',
+  component: SelectWithoutControl,
+} as Meta
+
+const Template: Story<SelectWithoutControlProps> = (args) => <SelectWithoutControl {...args} />
+
+const identityOptions = [
+  {
+    label: 'Anonymous',
+    value: 'ANONYMOUS',
+  },
+  {
+    label: 'JWT',
+    value: 'JWT',
+  },
+  {
+    label: 'Manual',
+    value: 'MANUAL',
+  },
+]
+
+const groupedOptions = [
+  ...identityOptions,
+  {
+    options: [
+      {
+        label: "I'm another group!",
+        value: 'x',
+        shouldStopPropagation: true,
+        onClick: () => console.log('clicked'),
+      },
+    ],
+  },
+]
+
+export const Primary = Template.bind({})
+Primary.args = {
+  options: identityOptions,
+}
+
+export const PrimaryGrouped = Template.bind({})
+PrimaryGrouped.args = {
+  options: groupedOptions,
+  value: 'option1',
+}
+
+export const PrimaryWithLabel = Template.bind({})
+PrimaryWithLabel.args = {
+  options: identityOptions,
+  label: 'Organization',
+}
+
+export const PrimaryDisabled = Template.bind({})
+PrimaryDisabled.args = {
+  options: identityOptions,
+  disabled: true,
+}
+
+export const PrimaryDisabledWithLabel = Template.bind({})
+PrimaryDisabledWithLabel.args = {
+  options: identityOptions,
+  label: 'Organization',
+  disabled: true,
+}
+
+export const PrimaryWithDefaultValue = Template.bind({})
+PrimaryWithDefaultValue.args = {
+  options: identityOptions,
+  label: 'Organization',
+  defaultValue: identityOptions.find((option) => option.value === 'MANUAL'),
+}
+
+export const PrimaryWithDefaultValueDisabled = Template.bind({})
+PrimaryWithDefaultValueDisabled.args = {
+  options: identityOptions,
+  label: 'Organization',
+  defaultValue: identityOptions.find((option) => option.value === 'MANUAL'),
+  disabled: true,
+}
+
+export const WithContainer = () => {
+  return (
+    <div
+      style={{
+        width: 170,
+      }}
+    >
+      <SelectWithoutControl
+        {...{
+          options: identityOptions,
+        }}
+      />
+    </div>
+  )
+}
