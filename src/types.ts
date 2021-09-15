c47bebb37be07d7571fd136b0bca47370ac5afdc
authereal <authereal@users.noreply.github.com>
2021-09-15 16:14:25 -0400

    Fix DirectoryBrowserGrid typing

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,6 @@
-export type UserAttr = {
-  department: string
-  manager: string
-  title: string
-}
-
-export interface UserRoles {
-  [x: string]: Array<string>
-}
-
-export interface Identities {
-  [x: string]: string
-}
-export interface Scopes {
-  [x: string]: string
-}
-
 export type User = {
   id: string
-  attr: UserAttr
-  created_at: string
-  updated_at: string
   display_name: string
-  enabled: boolean
   email: string
   picture: string
-  roles: UserRoles
-  identities: Identities
-  scopes: Scopes
 }
