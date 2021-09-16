0432ae864a95641dcdde3f9a2747a71a312e1cc0
Eduardo <drdpedroso@gmail.com>
2021-09-16 18:44:08 -0300

    Add disabled state to checkbox and name prop

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { theme } from '../../theme'
 import { Label } from '../Label'
 
@@ -21,7 +21,13 @@ const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
   width: 1px;
 `
 
-const StyledCheckbox = styled.div<{ checked?: boolean }>`
+const Icon = styled.svg`
+  fill: none;
+  stroke: ${theme.fullWhite};
+  stroke-width: 2px;
+`
+
+const StyledCheckbox = styled.div<{ checked?: boolean, $disabled?: boolean }>`
   display: inline-block;
   border: 1px solid ${props => props.checked ? theme.primary : theme.grey50};;
   margin-right: 10px;
@@ -33,12 +39,14 @@ const StyledCheckbox = styled.div<{ checked?: boolean }>`
   ${HiddenCheckbox}:focus + & {
     box-shadow: 0 0 0 3px ${theme.grey40};
   }
-`
-
-const Icon = styled.svg`
-  fill: none;
-  stroke: ${theme.fullWhite};
-  stroke-width: 2px;
+  ${({$disabled}) => $disabled ? css`
+    pointer-events: none;
+    background-color: ${theme.grey10};
+    border-color: ${theme.grey30};
+    svg {
+      stroke: ${theme.grey40};
+    }
+  ` : ''};
 `
 
 const HorizontalLabel = styled(Label)`
@@ -51,14 +59,16 @@ export type CheckboxProps = {
   checked?: boolean
   onChange: (checked: boolean) => void
   label?: string
+  name?: string
+  disabled?: boolean
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, label }) => {
+export const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, label, name, disabled }) => {
   return <CheckboxContainer>
     <HorizontalLabel $small>
-      <HiddenCheckbox type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)}/>
-      <StyledCheckbox checked={checked}>
-        {checked && <Icon viewBox="0 0 24 24">
+      <HiddenCheckbox disabled={disabled} name="name" checked={checked} onChange={(e) => onChange(e.target.checked)}/>
+      <StyledCheckbox $disabled={disabled} checked={checked}>
+        {checked && <Icon  viewBox="0 0 24 24">
           <polyline points="20 6 9 17 4 12" />
         </Icon>}
       </StyledCheckbox>
