7ab01b2d304a7ab36b6c63d932b7f84f7ed53f56
Eduardo Pedroso <drdpedroso@gmail.com>
2021-10-04 06:25:45 -0300

    Add native props to Input and Textarea (#57)
    * Add native props to Input and Textarea
    
    * remove unecessary props
    
    * 4.4.1

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -2,29 +2,12 @@ import React from 'react'
 import { FormControl, FormControlProps } from 'react-bootstrap'
 import './TextArea.css'
 
-export type TextAreaProps = {
+export type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
   value: string | number
   onChange?: FormControlProps['onChange']
-  rows?: number
-  placeholder?: string
   style?: unknown
 }
 
-export const TextArea: React.FC<TextAreaProps> = ({
-  placeholder,
-  value,
-  onChange,
-  rows,
-  style,
-  ...props
-}) => (
-  <FormControl
-    as="textarea"
-    rows={rows}
-    placeholder={placeholder}
-    value={value}
-    onChange={onChange}
-    style={style}
-    {...props}
-  />
+export const TextArea: React.FC<TextAreaProps> = ({ value, onChange, style, ...props }) => (
+  <FormControl as="textarea" value={value} onChange={onChange} style={style} {...props} />
 )
